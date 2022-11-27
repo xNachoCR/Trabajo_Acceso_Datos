72e964a5b054f623f765bd5dc576b99a7274fc03
xNachoCR <nacho.crubio@gmail.com>
2022-11-27 12:57:37 +0100

    Empleados
    Creación de la clase Empleados que almacena toda la información de cada uno de los empleados

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -1,10 +1,17 @@
 package proyecto;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String rutaFicherdat = "E:" + File.separator + "Documentos" 
+				+ File.separator + "DAM" + File.separator + "Segundo Curso" 
+				+ File.separator + "Acceso a Datos" + File.separator + "trabajoAcceso"
+				+ File.separator + "01102022.dat"; //Ruta local del fichero binario en mi PC
+		
+		
 	}
 
 }
