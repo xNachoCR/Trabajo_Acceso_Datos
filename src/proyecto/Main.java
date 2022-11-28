4d40b97b7248029ba6c5ce0da932a8d665f4723d
xNachoCR <nacho.crubio@gmail.com>
2022-11-28 10:53:50 +0100

    borrador final
    Borrador final, solo faltan pequeños detalles de funcionalidad y comentarios en el código

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -5,11 +5,23 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import reverse.Listaempleados;
+import reverse.ListaempleadosId;
 
 public class Main {
+	
+	private static SessionFactory hbsf;
 
 	public static void main(String[] args) throws IOException, SQLException {
 		// TODO Auto-generated method stub
+		
 		ArrayList<Empleados> totalMes = new ArrayList<Empleados>();
 		ArrayList<Empleados> desgloseMes = new ArrayList<Empleados>();
 		
@@ -24,12 +36,44 @@ public class Main {
 		totalMes = pf.procesaFichero(rutaFicherdat);
 		desgloseMes = pb.procesaDatos();
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce fecha para generar registro(yyyy-MM-dd): ");
+		String fecha = sc.nextLine();
+		
 		actualizaDatos(totalMes, desgloseMes);
 		
+		introduceFecha(totalMes, fecha);
+		
+		System.out.println("Datos que se introducirán en la base de datos...");
 		
 		for (int i = 0; i < totalMes.size(); i++) {
 			System.out.println(totalMes.get(i));
 		}
+		
+		Configuration config = new Configuration();
+		config.configure();
+		SessionFactory session = config.buildSessionFactory();
+		Session ss = session.openSession();
+		Transaction transaction = ss.beginTransaction();
+		
+		for(int i = 0; i < totalMes.size(); i++) {
+			ListaempleadosId empList = new ListaempleadosId();
+			Listaempleados list = new Listaempleados();
+			
+			empList.setMatricula(totalMes.get(i).getMatricula());
+			empList.setDepartamento(totalMes.get(i).getDepartamento());
+			empList.setApenom(totalMes.get(i).getNombre());
+			empList.setTotventas(totalMes.get(i).getVentasSem());
+			empList.setTotgastos(totalMes.get(i).getGastos());
+			String dt = totalMes.get(i).getSemana().toString();
+			empList.setFecha(java.sql.Date.valueOf(dt));
+			
+			list.setId(empList);
+			ss.save(list);
+		}
+		
+		transaction.commit();
+		session.close();
 	}
 	
 	private static void actualizaDatos(ArrayList<Empleados> totalMes, ArrayList<Empleados> desgloseMes) {
@@ -45,6 +89,12 @@ public class Main {
 				totalMes.get(i).setNombre(desgloseMes.get(i).getNombre());
 				totalMes.get(i).setDepartamento(desgloseMes.get(i).getDepartamento());
 			}
+		}
+	}
+	
+	private static void introduceFecha(ArrayList<Empleados> totalMes, String fecha) {
+		for (int i = 0; i < totalMes.size(); i++) {
+			totalMes.get(i).setSemana(java.sql.Date.valueOf(fecha));
 		}
 	}
 	
